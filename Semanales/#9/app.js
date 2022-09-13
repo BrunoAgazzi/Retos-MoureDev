a6c3677139726fe3aa71b08687446e76b94aa72a
Bruno Agazzi <63024223+BrunoAgazzi@users.noreply.github.com>
2022-09-13 19:15:39 -0300

    #9

--- Semanales/#9/app.js
+++ Semanales/#9/app.js
@@ -8,3 +8,82 @@
  * - El alfabeto morse soportado será el mostrado en
  *   https://es.wikipedia.org/wiki/Código_morse.
  */
+
+
+
+const morseConverter = strg =>{
+
+    const codeMorse = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "CH": "----",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+        "0": "-----",
+        "1": ".----",
+        "2": "..---",
+        "3": "...---",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        ".": ".-.-.-",
+        ",": "--..--",
+        "?": "..--..",
+        '"': ".-..-.",
+        "/": "—··—·",
+        " ": " ",
+    }
+    const caracteres = /[a-zA-Z0-9]/;
+    
+    let typeCode = caracteres.test(strg);
+    if(typeCode){
+         //alf to morse
+         let cMorse = '';
+         for(let i = 0; i < strg.length; i++){
+             let letter = strg.charAt(i).toUpperCase();
+             cMorse = cMorse + codeMorse[letter] + " ";
+         }
+         console.log(cMorse);
+    }else{
+         //morse to alf
+         let alfString = '';
+         let arrLetter = strg.split(' ');
+         for(let i = 0; i < arrLetter.length; i++){
+             let letterMorse = arrLetter[i];
+             let letter = Object.keys(codeMorse).find(key => codeMorse[key] === letterMorse);
+             if(letter)alfString = alfString + letter ;
+             else alfString = alfString + ' ';
+         }
+         console.log(alfString.toLowerCase())
+      
+    }
+}
+morseConverter('sos');
+morseConverter('... --- ...');
+
+
+
