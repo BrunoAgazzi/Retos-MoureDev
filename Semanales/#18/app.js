82956835f1e4fe20a6797c894beeb86afec784c3
Bruno Agazzi <63024223+BrunoAgazzi@users.noreply.github.com>
2022-09-15 17:36:47 -0300

    Create app.js

---
+++ Semanales/#18/app.js
@@ -0,0 +1,22 @@
+/*
+ * Crea una función que analice una matriz 3x3 compuesta por "X" y "O"
+ * y retorne lo siguiente:
+ * - "X" si han ganado las "X"
+ * - "O" si han ganado los "O"
+ * - "Empate" si ha habido un empate
+ * - "Nulo" si la proporción de "X", de "O", o de la matriz no es correcta.
+ *   O si han ganado los 2.
+ * Nota: La matriz puede no estar totalmente cubierta.
+ * Se podría representar con un vacío "", por ejemplo.
+ */
+
+
+console.tablet([
+    
+]);
+
+const myObj = { 
+    0: { apples: 2, oranges: 4, bananas: 7, melons: 0 }, 
+    1: { apples: 0, oranges: 10, bananas: 0, melons: 0 }, 
+    2: { apples: 0, oranges: 0, bananas: 0, melons: 5 } 
+}
