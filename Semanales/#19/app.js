cbe5042d0eb7953f78489cf4fa6033981da12688
Bruno Agazzi <63024223+BrunoAgazzi@users.noreply.github.com>
2022-09-16 01:48:06 -0300

    #19

--- Semanales/#19/app.js
+++ Semanales/#19/app.js
@@ -2,3 +2,17 @@
  * Crea una función que reciba días, horas, minutos y segundos (como enteros)
  * y retorne su resultado en milisegundos.
  */
+
+
+
+const changeHours = (days,hours,minutes,seconds) =>{
+
+    let msInD = ((((days*24)*60)*60)*1000);
+    let msInH = (((hours*60)*60)*1000);
+    let msInM = ((minutes * 60)*1000);
+    let msInS = (seconds *1000);
+    console.log(msInD+msInH+msInM+msInS+'ms');
+
+}
+
+changeHours(1,2,20,40)
